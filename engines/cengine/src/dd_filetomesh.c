707366a2c35c14088d6ae9bb2729420dd09c7ead
Tom Tsagk <tomtsagk@darkdimension.org>
2021-08-21 10:57:49 +0100

    Handle buffer scanning better

--- engines/cengine/src/dd_filetomesh.c
+++ engines/cengine/src/dd_filetomesh.c
@@ -654,8 +654,9 @@ int dd_load_ply(struct dd_loaded_mesh *m, const char *path, int settings) {
 				property vertex_indices
 	 */
 
+	buff[1023] = '\0';
 	//Read words until end of file or end of header
-	while ( fscanf(f, "%1024s", buff) != EOF
+	while ( fscanf(f, "%1022s", buff) != EOF
 		&& strcmp(buff, "end_header") != 0 )
 	{
 		// Found comment - Skip line
@@ -671,7 +672,8 @@ int dd_load_ply(struct dd_loaded_mesh *m, const char *path, int settings) {
 			elements[elementCurrent].propertyCurrent = -1;
 
 			//Get next word
-			if ( fscanf(f, "%1024s", buff) == EOF )
+			buff[1023] = '\0';
+			if ( fscanf(f, "%1022s", buff) == EOF )
 				goto error;
 
 			strncpy(elements[elementCurrent].name, buff, 99);
@@ -708,13 +710,15 @@ int dd_load_ply(struct dd_loaded_mesh *m, const char *path, int settings) {
 			struct ply_property *property = &element->p[element->propertyCurrent];
 
 			// Get format/list
-			if ( fscanf(f, "%s", buff) == EOF )
+			buff[1023] = '\0';
+			if ( fscanf(f, "%1022s", buff) == EOF )
 				goto error;
 
 			// if list, get list's format
 			property->list_format = PLY_FORMAT_NONE;
 			if (strcmp(buff, "list") == 0) {
-				if ( fscanf(f, "%s", buff) == EOF )
+				buff[1023] = '\0';
+				if ( fscanf(f, "%1022s", buff) == EOF )
 					goto error;
 
 				for (int i = 0; i < formats_total; i++) {
@@ -725,8 +729,10 @@ int dd_load_ply(struct dd_loaded_mesh *m, const char *path, int settings) {
 				}
 
 				// next word is format
-				if ( fscanf(f, "%s", buff) == EOF )
+				buff[1023] = '\0';
+				if ( fscanf(f, "%1022s", buff) == EOF ) {
 					goto error;
+				}
 			}
 
 			// get format or property
@@ -739,8 +745,10 @@ int dd_load_ply(struct dd_loaded_mesh *m, const char *path, int settings) {
 			}
 
 			// Get property name
-			if ( fscanf(f, "%s", buff) == EOF )
+			buff[1023] = '\0';
+			if ( fscanf(f, "%1022s", buff) == EOF ) {
 				goto error;
+			}
 
 			strncpy(property->name, buff, 99);
 			property->name[99] = '\0';
@@ -977,7 +985,8 @@ int dd_load_obj(struct dd_loaded_mesh *m, const char *path, int settings) {
 	char buff[1024];
 
 	//Get the first character of each line
-	while ( fscanf(f, "%1024s", buff) != EOF ) {
+	buff[1023] = '\0';
+	while ( fscanf(f, "%1022s", buff) != EOF ) {
 		//Vertex
 		if (buff[0] == 'v') {
 			//Get vertex xyz
@@ -1035,10 +1044,12 @@ int dd_load_obj(struct dd_loaded_mesh *m, const char *path, int settings) {
 		//Unsupported element
 		else {
 			//Skip line
-			if ( fscanf(f, "%1024[^\n]%*1c", buff) == EOF ) {
+			buff[1023] = '\0';
+			if ( fscanf(f, "%1022[^\n]%*1c", buff) == EOF ) {
 				goto error;
 			}
 		}
+		buff[1023] = '\0';
 	}
 
 	//Close file
