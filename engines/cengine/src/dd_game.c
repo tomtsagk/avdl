1aba9a672f06ddf0ac1b81ea518dbcb6c6064d75
Tom Tsagk <tomtsagk@darkdimension.org>
2022-02-07 23:23:37 +0000

    Remove dependency on `whereami` on the `cengine`

--- engines/cengine/src/dd_game.c
+++ engines/cengine/src/dd_game.c
@@ -75,7 +75,6 @@ void dd_fullscreenToggle() {
 	#endif
 }
 
-char *dynamicProjectLocation = 0;
 extern char *installLocation = "";
 extern int installLocationDynamic;
 
@@ -100,12 +99,7 @@ const char *avdl_getProjectLocation() {
 		return dynamicProjectLocationW;
 	}
 	#else
-	if (!dynamicProjectLocation) {
-		return "";
-	}
-	else {
-		return dynamicProjectLocation;
-	}
+	return "";
 	#endif
 
 	#else
@@ -147,47 +141,8 @@ void avdl_initProjectLocation() {
 	// not supported on non-windows os for now
 	#endif
 
-	/*
-	int length = wai_getExecutablePath(0, 0, 0);
-	dynamicProjectLocation = malloc(sizeof(char) *(length+1));
-	if (!dynamicProjectLocation) {
-		printf("avdl error: unable to allocate memory for dynamic package location\n");
-	}
-	wai_getExecutablePath(dynamicProjectLocation, length, 0);
-	dynamicProjectLocation[length] = '\0';
-
-	int directoriesToSkip = 1;
-	for (int i = length-1; i >= 0; i--) {
-		#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-		if (dynamicProjectLocation[i] == '\\') {
-		#else
-		if (dynamicProjectLocation[i] == '/') {
-		#endif
-			if (directoriesToSkip > 0) {
-				directoriesToSkip--;
-			}
-			else {
-				dynamicProjectLocation[i+1] = '\0';
-				directoriesToSkip = -1;
-				break;
-			}
-		}
-	}
-
-	if (directoriesToSkip != -1) {
-		printf("avdl error: unable to parse dynamic package location: %s\n", dynamicProjectLocation);
-		free(dynamicProjectLocation);
-		dynamicProjectLocation = 0;
-		return;
-	}
-	*/
 	#endif
 }
 
 void avdl_cleanProjectLocation() {
-	#ifdef AVDL_DYNAMIC_PKG_LOCATION
-	if (dynamicProjectLocation) {
-		free(dynamicProjectLocation);
-	}
-	#endif
 }
