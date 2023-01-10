183c8b9754b9f5230957a098b70d65eec455a3fc
Tom Tsagk <tomtsagk@afloofdev.com>
2023-01-10 13:26:43 +0000

    Add `--standalone` for custom builds

--- src/avdl_main.c
+++ src/avdl_main.c
@@ -50,6 +50,7 @@ int create_android_directory(const char *androidDirName);
 int avdlSteamMode = 0;
 
 int avdlQuietMode = 0;
+int avdlStandalone = 0;
 
 struct cengine_file_structure {
 	const char *main;
@@ -321,6 +322,10 @@ int main(int argc, char *argv[]) {
 				else
 				if (strcmp(argv[i], "--steam") == 0) {
 					avdlSteamMode = 1;
+				}
+				else
+				if (strcmp(argv[i], "--standalone") == 0) {
+					avdlStandalone = 1;
 				}
 				// unknown double dash argument
 				else {
@@ -933,8 +938,13 @@ int main(int argc, char *argv[]) {
 				strcat(buffer, additionalLibDirectory[i]);
 			}
 
-			//strcat(buffer, " -O3 -lm -logg -lopus -lopusfile -lpng -w -lSDL2 -lSDL2_mixer -lpthread -lGL -lGLEW");
-			strcat(buffer, " -O3 -lm -l:libogg.so.0 -l:libopus.so.0 -l:libopusfile.so.0 -l:libpng16.so.16 -w -l:libSDL2-2.0.so.0 -l:libSDL2_mixer-2.0.so.0 -lpthread -lGL -l:libGLEW.so.2.2");
+			if (avdlStandalone) {
+				strcat(buffer, " -O3 -lm -l:libogg.so.0 -l:libopus.so.0 -l:libopusfile.so.0 -l:libpng16.so.16 -w -l:libSDL2-2.0.so.0 -l:libSDL2_mixer-2.0.so.0 -lpthread -lGL -l:libGLEW.so.2.2");
+			}
+			else {
+				strcat(buffer, " -O3 -lm -logg -lopus -lopusfile -lpng -w -lSDL2 -lSDL2_mixer -lpthread -lGL -lGLEW");
+			}
+
 			if (avdlSteamMode) {
 				strcat(buffer, " -lsteam_api ");
 			}
