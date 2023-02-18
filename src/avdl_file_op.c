816028c9d88fae495903e265562bbaf38116b545
Tom Tsagk <tomtsagk@afloofdev.com>
2023-02-18 21:39:42 +0000

    Attempt stackoverflow solution for windows directory recursion

--- src/avdl_file_op.c
+++ src/avdl_file_op.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "avdl_file_op.h"
 #include "avdl_settings.h"
@@ -390,6 +392,48 @@ int Avdl_FileOp_GetNumberOfFiles(const char *directory) {
 int Avdl_FileOp_ForFileInDirectory(const char *dirname, int (*handle_function)(const char *dirname, const char *filename, int, int)) {
 
 	#if AVDL_IS_OS(AVDL_OS_WINDOWS)
+
+	WIN32_FIND_DATA fdFile;
+	HANDLE hFind = NULL;
+
+	char sPath[2048];
+
+	//Specify a file mask. *.* = We want everything!
+	sprintf(sPath, "%s\\*.*", dirname);
+
+	if((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+	{
+		printf("Path not found: [%s]\n", dirname);
+		return -1;
+	}
+
+	do
+	{
+		//Find first file will always return "."
+		//    and ".." as the first two directories.
+		if(strcmp(fdFile.cFileName, ".") != 0
+		&& strcmp(fdFile.cFileName, "..") != 0)
+		{
+			//Build up our file path using the passed in
+			//  [sDir] and the file/foldername we just found:
+			sprintf(sPath, "%s\\%s", dirname, fdFile.cFileName);
+
+			//Is the entity a File or Folder?
+			if(fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+			{
+				printf("Directory: %s\n", sPath);
+				//ListDirectoryContents(sPath); //Recursion, I love it!
+			}
+			else{
+				printf("File: %s\n", sPath);
+				handle_function(dirname, fdFile.cFileName, 5, 10);
+			}
+		}
+	}
+	while(FindNextFile(hFind, &fdFile)); //Find the next file.
+
+	FindClose(hFind); //Always, Always, clean things up!
+
 	return 0;
 	#else
 
