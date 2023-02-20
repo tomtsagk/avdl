81bbd55cb1451018e2b91b57559d88beeab14cda
Tom Tsagk <tomtsagk@afloofdev.com>
2023-02-20 21:32:22 +0000

    Change to using `SDL_PollEvent`, which handles input better

--- engines/cengine/src/main.c
+++ engines/cengine/src/main.c
@@ -99,22 +99,6 @@ pthread_mutex_t jniMutex;
 int avdl_state_initialised = 0;
 int avdl_state_active = 0;
 
-#if DD_PLATFORM_NATIVE
-Uint32 GameLoopTimer(Uint32 interval, void *param) {
-	// Create a user event to call the game loop.
-	SDL_Event event;
-
-	event.type = SDL_USEREVENT;
-	event.user.code = 1;
-	event.user.data1 = 0;
-	event.user.data2 = 0;
-
-	SDL_PushEvent(&event);
-
-	return interval;
-}
-#endif
-
 struct avdl_achievements *achievements = 0;
 
 int avdl_verify = 0;
@@ -241,9 +225,6 @@ int dd_main(int argc, char *argv[]) {
 		}
 		handleResize(dd_window_width(), dd_window_height());
 
-		// trigger an update event
-		timer = SDL_AddTimer(30, GameLoopTimer, 0);
-
 		// init opengl
 		GLenum glewError = glewInit();
 		if (glewError != GLEW_OK) {
@@ -328,75 +309,77 @@ int dd_main(int argc, char *argv[]) {
 		// start the loop
 		int isRunning = 1;
 		SDL_Event event;
-		while (isRunning && SDL_WaitEvent(&event) && !dd_flag_exit) {
-			switch (event.type) {
-				case SDL_USEREVENT:
-					update();
-					break;
-				case SDL_QUIT:
-					isRunning = 0;
-					break;
-				case SDL_WINDOWEVENT:
-					if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-						handleResize(event.window.data1, event.window.data2);
-					}
-					break;
-				case SDL_MOUSEMOTION:
-					handlePassiveMotion(event.motion.x, event.motion.y);
-					break;
-				case SDL_MOUSEBUTTONDOWN:
-					handleMousePress(0, 0, event.motion.x, event.motion.y);
-					break;
-				case SDL_MOUSEBUTTONUP:
-					handleMousePress(0, 1, event.motion.x, event.motion.y);
-					break;
-				case SDL_KEYDOWN:
-					// temporary keyboard controls
-					if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-						handleKeyboardPress(27, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_A) {
-						handleKeyboardPress(97, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_D) {
-						handleKeyboardPress(100, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_W) {
-						handleKeyboardPress(119, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_S) {
-						handleKeyboardPress(115, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
-						handleKeyboardPress(32, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
-						handleKeyboardPress(13, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
-						handleKeyboardPress(1, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_UP) {
-						handleKeyboardPress(2, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
-						handleKeyboardPress(3, 0, 0);
-					}
-					else
-					if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-						handleKeyboardPress(4, 0, 0);
-					}
-					break;
+		//while (isRunning && SDL_WaitEvent(&event) && !dd_flag_exit) {
+		while (isRunning && !dd_flag_exit) {
+			while (SDL_PollEvent(&event)) {
+				switch (event.type) {
+					case SDL_QUIT:
+						isRunning = 0;
+						break;
+					case SDL_WINDOWEVENT:
+						if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+							handleResize(event.window.data1, event.window.data2);
+						}
+						break;
+					case SDL_MOUSEMOTION:
+						handlePassiveMotion(event.motion.x, event.motion.y);
+						break;
+					case SDL_MOUSEBUTTONDOWN:
+						handleMousePress(0, 0, event.motion.x, event.motion.y);
+						break;
+					case SDL_MOUSEBUTTONUP:
+						handleMousePress(0, 1, event.motion.x, event.motion.y);
+						break;
+					case SDL_KEYDOWN:
+						// temporary keyboard controls
+						if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+							handleKeyboardPress(27, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_A) {
+							handleKeyboardPress(97, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_D) {
+							handleKeyboardPress(100, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_W) {
+							handleKeyboardPress(119, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_S) {
+							handleKeyboardPress(115, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
+							handleKeyboardPress(32, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
+							handleKeyboardPress(13, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
+							handleKeyboardPress(1, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_UP) {
+							handleKeyboardPress(2, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
+							handleKeyboardPress(3, 0, 0);
+						}
+						else
+						if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
+							handleKeyboardPress(4, 0, 0);
+						}
+						break;
+				}
 			}
+			update();
+			SDL_Delay(33.333);
 		}
 	}
 
