f75ca3a88ef0be268ff89ea5a0db7cf868d4a932
Tom Tsagk <tomtsagk@afloofdev.com>
2023-03-13 20:23:37 +0000

    Improve `dd_vec3` functions

--- engines/cengine/include/dd_vec3.h
+++ engines/cengine/include/dd_vec3.h
@@ -3,16 +3,22 @@
 
 struct dd_vec3 {
 	float x, y, z;
-	void (*set)(struct dd_vec3 *, float, float, float);
+	void (*setf)(struct dd_vec3 *, float, float, float);
 	float (*getX)(struct dd_vec3 *);
 	float (*getY)(struct dd_vec3 *);
 	float (*getZ)(struct dd_vec3 *);
 };
 
 void dd_vec3_create(struct dd_vec3 *o);
-void dd_vec3_set(struct dd_vec3 *o, float x, float y, float z);
-void dd_vec3_add(struct dd_vec3 *o1, float x, float y, float z);
-void dd_vec3_addVec3(struct dd_vec3 *o1, struct dd_vec3 *o2);
+
+void dd_vec3_set(struct dd_vec3 *o, struct dd_vec3 *src);
+void dd_vec3_setf(struct dd_vec3 *o, float x, float y, float z);
+
+void dd_vec3_add(struct dd_vec3 *o, struct dd_vec3 *a, struct dd_vec3 *b);
+void dd_vec3_addf(struct dd_vec3 *o1, float x, float y, float z);
+
+void dd_vec3_substract(struct dd_vec3 *o, struct dd_vec3 *a, struct dd_vec3 *b);
+void dd_vec3_substractf(struct dd_vec3 *o, float x, float y, float z);
 
 float dd_vec3_getX(struct dd_vec3 *o);
 float dd_vec3_getY(struct dd_vec3 *o);
--- engines/cengine/src/dd_vec3.c
+++ engines/cengine/src/dd_vec3.c
@@ -5,13 +5,20 @@ void dd_vec3_create(struct dd_vec3 *o) {
 	o->x = 0;
 	o->y = 0;
 	o->z = 0;
-	o->set = dd_vec3_set;
+	//o->set = dd_vec3_set;
+	o->setf = dd_vec3_setf;
 	o->getX = dd_vec3_getX;
 	o->getY = dd_vec3_getY;
 	o->getZ = dd_vec3_getZ;
 }
 
-void dd_vec3_set(struct dd_vec3 *o, float x, float y, float z) {
+void dd_vec3_set(struct dd_vec3 *o, struct dd_vec3 *src) {
+	o->x = src->x;
+	o->y = src->y;
+	o->z = src->z;
+}
+
+void dd_vec3_setf(struct dd_vec3 *o, float x, float y, float z) {
 	o->x = x;
 	o->y = y;
 	o->z = z;
@@ -30,20 +37,20 @@ float dd_vec3_getZ(struct dd_vec3 *o) {
 void dd_vec3_clean(struct dd_vec3 *o) {
 }
 
-void dd_vec3_add(struct dd_vec3 *o1, float x, float y, float z) {
+void dd_vec3_addf(struct dd_vec3 *o1, float x, float y, float z) {
 	o1->x += x;
 	o1->y += y;
 	o1->z += z;
 }
 
-void dd_vec3_addVec3(struct dd_vec3 *o1, struct dd_vec3 *o2) {
-	o1->x += o2->x;
-	o1->y += o2->y;
-	o1->z += o2->z;
+void dd_vec3_add(struct dd_vec3 *o, struct dd_vec3 *a, struct dd_vec3 *b) {
+	o->x = a->x +b->x;
+	o->y = a->y +b->y;
+	o->z = a->z +b->z;
 }
 
 void dd_vec3_cross(struct dd_vec3 *o, struct dd_vec3 *v1, struct dd_vec3 *v2) {
-	dd_vec3_set(o,
+	dd_vec3_setf(o,
 		dd_vec3_getY(v1) *dd_vec3_getZ(v2) -dd_vec3_getZ(v1) *dd_vec3_getY(v2),
 		dd_vec3_getZ(v1) *dd_vec3_getX(v2) -dd_vec3_getX(v1) *dd_vec3_getZ(v2),
 		dd_vec3_getX(v1) *dd_vec3_getY(v2) -dd_vec3_getY(v1) *dd_vec3_getX(v2)
@@ -63,4 +70,16 @@ float dd_vec3_magnitude(struct dd_vec3 *o) {
 
 float dd_vec3_dot(struct dd_vec3 *a, struct dd_vec3 *b) {
 	return a->x *b->x + a->y *b->y + a->z *b->z;
+}
+
+void dd_vec3_substract(struct dd_vec3 *o, struct dd_vec3 *a, struct dd_vec3 *b) {
+	o->x = a->x -b->x;
+	o->y = a->y -b->y;
+	o->z = a->z -b->z;
+}
+
+void dd_vec3_substractf(struct dd_vec3 *o, float x, float y, float z) {
+	o->x -= x;
+	o->y -= y;
+	o->z -= z;
 }
