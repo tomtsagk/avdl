49b493e184b0f5b00450db02922a9d30dc5aba84
Tom Tsagk <tomtsagk@afloofdev.com>
2023-04-18 20:29:24 +0100

    Add time measurement for Windows

--- include/avdl_time.h
+++ include/avdl_time.h
@@ -4,10 +4,16 @@
 #include <time.h>
 
 struct avdl_time {
+	#ifdef AVDL_IS_OS(AVDL_OS_LINUX)
 	struct timespec start;
 	struct timespec end;
 	double start_sec;
 	double end_sec;
+	#elif AVDL_IS_OS(AVDL_OS_WINDOWS)
+	LARGE_INTEGER frequency;
+	LARGE_INTEGER start;
+	LARGE_INTEGER end;
+	#endif
 	double elapsed_sec;
 };
 
--- src/avdl_time.c
+++ src/avdl_time.c
@@ -3,6 +3,9 @@
 void avdl_time_start(struct avdl_time *o) {
 	#ifdef AVDL_IS_OS(AVDL_OS_LINUX)
 	clock_gettime(CLOCK_REALTIME, &o->start);
+	#elif AVDL_IS_OS(AVDL_OS_WINDOWS)
+	QueryPerformanceFrequency(&o->frequency);
+	QueryPerformanceCounter(&o->start);
 	#endif
 }
 
@@ -14,11 +17,16 @@ void avdl_time_end(struct avdl_time *o) {
 	o->start_sec = o->start.tv_sec + o->start.tv_nsec / NANO_PER_SEC;
 	o->end_sec = o->end.tv_sec + o->end.tv_nsec / NANO_PER_SEC;
 	o->elapsed_sec = o->end_sec - o->start_sec;
+	#elif AVDL_IS_OS(AVDL_OS_WINDOWS)
+	QueryPerformanceCounter(&o->end);
+	o->elapsed_sec = (double)(o->end.QuadPart -o->start.QuadPart) / o->frequency.QuadPart;
 	#endif
 }
 
 double avdl_time_getTimeDouble(struct avdl_time *o) {
 	#ifdef AVDL_IS_OS(AVDL_OS_LINUX)
+	return o->elapsed_sec;
+	#elif AVDL_IS_OS(AVDL_OS_WINDOWS)
 	return o->elapsed_sec;
 	#else
 	return 0;
