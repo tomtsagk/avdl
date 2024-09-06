bf9403729db1a9b3a5423195d2ab07cdde143cb4
Tom Tsagkatos <TomTsagkatos@afloof.dev>
2024-09-06 23:35:18 +0100

    Tidy up docker files and android dependencies

--- scripts/dependencies_android.sh
+++ scripts/dependencies_android.sh
@@ -11,9 +11,9 @@ wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_la
 unzip commandlinetools-linux-11076708_latest.zip
 mv cmdline-tools/* android/cmdline-tools/latest/
 yes | ./android/cmdline-tools/latest/bin/sdkmanager --licenses
-./android/cmdline-tools/latest/bin/sdkmanager --install "ndk;25.1.8937393"
+./android/cmdline-tools/latest/bin/sdkmanager --install "ndk;27.1.12297006"
 ./android/cmdline-tools/latest/bin/sdkmanager --install "platform-tools"
 ./android/cmdline-tools/latest/bin/sdkmanager --install "build-tools;30.0.3"
-./android/cmdline-tools/latest/bin/sdkmanager --install "platforms;android-33"
+./android/cmdline-tools/latest/bin/sdkmanager --install "platforms;android-34"
 
 git clone https://github.com/libsdl-org/freetype.git /avdl/freetype_quest2
