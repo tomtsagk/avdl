cc878b400fe9e03aa486fb3b448bb1a6dd33cabf
Tom Tsagkatos <TomTsagkatos@afloof.dev>
2024-09-12 17:52:46 +0100

    Windows buid script now needs to be passed avdl build

--- scripts/avdl_project_windows.bat
+++ scripts/avdl_project_windows.bat
@@ -1,21 +1,18 @@
 
-:: required first argument
+:: required first argument - output directory
 if "%1"=="" (
-	echo "avdl_project_windows.bat: first argument is required as the output folder"
+	echo "avdl_project_windows.bat: first argument is required as the output directory"
+	exit
+)
+
+:: required second argument - avdl directory
+if "%2"=="" (
+	echo "avdl_project_windows.bat: second argument is required as the avdl directory"
 	exit
 )
 
 set start_directory=%cd%
 set "start_directory=%start_directory:\=/%"
-
-:: prepare avdl
-if not exist "avdl_windows.zip" (
-	if not "%2"=="" powershell.exe -Command "Invoke-WebRequest %2 -OutFile avdl_windows.zip"
-)
-if not exist AVDL_BUILD mkdir AVDL_BUILD
-unzip avdl_windows.zip -d AVDL_BUILD
-
-.\AVDL_BUILD\bin\avdl.exe --cmake
 
 :: get into the cache directory
 if not exist ".avdl_cache" mkdir ".avdl_cache"
@@ -27,14 +24,14 @@ cd "avdl_project_windows"
 :: imagemagick ?
 
 mkdir ..\..\cengine
-xcopy /e "..\..\AVDL_BUILD\share\avdl\cengine\" "..\..\cengine"
-xcopy /e "..\..\AVDL_BUILD\include\" "..\..\cengine"
+xcopy /e "%start_directory%\%2\share\avdl\cengine\" "..\..\cengine"
+xcopy /e "%start_directory%\%2\include\" "..\..\cengine"
 if "%3"=="" (
-	cmake ../../ . -DCMAKE_INSTALL_PREFIX="GAME_BUILD" -DAVDL_DIRECTORY="%start_directory%/AVDL_BUILD/"
+	cmake ../../ . -DCMAKE_INSTALL_PREFIX="GAME_BUILD" -DAVDL_DIRECTORY="%start_directory%/%2/"
 )
 
 if not "%3"=="" (
-	cmake ../../ . -DCMAKE_INSTALL_PREFIX="GAME_BUILD" -DAVDL_DIRECTORY="%start_directory%/AVDL_BUILD/" -DAVDL_STEAM=1
+	cmake ../../ . -DCMAKE_INSTALL_PREFIX="GAME_BUILD" -DAVDL_DIRECTORY="%start_directory%/%2/" -DAVDL_STEAM=1
 )
 
 cmake --build . --config Release
