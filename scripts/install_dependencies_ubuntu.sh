2189429c5e68f984aea41584686808cc84d260e1
Tom Tsagkatos <TomTsagkatos@afloof.dev>
2024-09-13 20:34:28 +0100

    Add `zip` on ubuntu dependencies

--- scripts/install_dependencies_ubuntu.sh
+++ scripts/install_dependencies_ubuntu.sh
@@ -3,7 +3,8 @@
 apt-get update -y
 apt-get upgrade -y
 
-apt-get install -y gcc pkg-config ca-certificates imagemagick wget unzip
+# avdl packaging
+apt-get install -y gcc pkg-config ca-certificates imagemagick wget unzip zip
 
 # sdl and sdl_mixer deps
 apt-get install -y build-essential  git  make  cmake  autoconf  automake  libtool  pkg-config  libasound2-dev  libpulse-dev  libaudio-dev  libjack-dev  libx11-dev  libxext-dev  libxrandr-dev  libxcursor-dev  libxfixes-dev  libxi-dev  libxss-dev  libgl1-mesa-dev  libdbus-1-dev  libudev-dev  libgles2-mesa-dev  libegl1-mesa-dev  libibus-1.0-dev  fcitx-libs-dev  libsamplerate0-dev  libsndio-dev  libwayland-dev  libxkbcommon-dev  libdrm-dev  libgbm-dev libssl-dev
