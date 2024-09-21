2b8de542446f15b3334ecb83cf23ce31dca96209
Tom Tsagkatos <TomTsagkatos@afloof.dev>
2024-09-21 21:31:27 +0100

    Fix cmake compilation

--- engines/cengine/cengine.cmake
+++ engines/cengine/cengine.cmake
@@ -1,10 +1,2 @@
-file(GLOB cengine_src CONFIGURE_DEPENDS
-	"engines/cengine/src/*"
-)
-
-file(GLOB cengine_headers CONFIGURE_DEPENDS
-	"engines/cengine/include/*.h"
-)
-
-install(FILES ${cengine_src} DESTINATION share/avdl/cengine)
-install(FILES ${cengine_headers} DESTINATION include)
+install(DIRECTORY "engines/cengine/src/" DESTINATION "share/avdl/cengine")
+install(DIRECTORY "engines/cengine/include/" DESTINATION "share/avdl/cengine")
